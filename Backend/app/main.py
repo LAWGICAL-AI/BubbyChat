ea8ff0054fb505b2b51fff0d601be56655f868e4
구동현 <dies5115@naver.com>
2024-10-17 16:24:34 +0900

    fix: server fix still in-progress

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,16 +1,25 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서 접근 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World hiiiiii"}
+    return {"Hello": "World?"}
 
 @app.get("/test")
 def testing():
-    return {55}
+    return {"value": 55}
 
 @app.get("/inference")
-def testing():
-    return {"return":"inference test"}
+def inference():
+    return {"return": "inference test"}
