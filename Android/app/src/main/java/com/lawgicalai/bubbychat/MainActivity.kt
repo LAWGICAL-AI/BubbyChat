05a31789162fe47681fc8e1d8f2da445320b9f4b
kimmandoo <mingyu5675@gmail.com>
2024-10-18 00:49:45 +0900

    chore: lib 최신화

--- Android/app/src/main/java/com/lawgicalai/bubbychat/MainActivity.kt
+++ Android/app/src/main/java/com/lawgicalai/bubbychat/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.lawgicalai.bubbychat.ui.theme.BubbyChat
+import com.lawgicalai.bubbychat.ui.theme.BubbyChatTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BubbyChat {
+            BubbyChatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    BubbyChat {
+    BubbyChatTheme {
         Greeting("Android")
     }
 }
