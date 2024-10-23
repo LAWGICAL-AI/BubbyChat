b185505e56aa3317c8db4b6032cfe4cd1cab21a8
kimmandoo <mingyu5675@gmail.com>
2024-10-23 23:21:29 +0900

    feat: font 추가

--- Android/app/src/main/java/com/lawgicalai/bubbychat/ui/theme/Type.kt
+++ Android/app/src/main/java/com/lawgicalai/bubbychat/ui/theme/Type.kt
@@ -1,34 +1,28 @@
 package com.lawgicalai.bubbychat.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.lawgicalai.bubbychat.R
 
-// Set of Material typography styles to start with
+// font는 Type에 있다(굳이 Type이 아니어도 되긴함)
+val pretendardFontFamily: FontFamily = FontFamily(Font(R.font.ptd_regular))
+
+val defaultTypography = Typography()
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = pretendardFontFamily),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = pretendardFontFamily),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = pretendardFontFamily),
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = pretendardFontFamily),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = pretendardFontFamily),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = pretendardFontFamily),
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = pretendardFontFamily),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = pretendardFontFamily),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = pretendardFontFamily),
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = pretendardFontFamily),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = pretendardFontFamily),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = pretendardFontFamily),
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = pretendardFontFamily),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = pretendardFontFamily),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = pretendardFontFamily),
 )
